384e7ab5a631ff881191d5e9f9b54241a9bb0c3a
machamp0714 <machamp0714@gmail.com>
2024-07-14 11:03:43 +0900

    Add test for exit code

--- hello/tests/cli.rs
+++ hello/tests/cli.rs
@@ -3,5 +3,17 @@ use assert_cmd::Command;
 #[test]
 fn works() {
     let mut cmd = Command::cargo_bin("hello").unwrap(); // Cargoによってビルドされたバイナリのパスを取得する
+    cmd.assert().success().stdout("Hello, world!\n");
+}
+
+#[test]
+fn true_ok() {
+    let mut cmd = Command::cargo_bin("hello").unwrap();
     cmd.assert().success();
+}
+
+#[test]
+fn false_not_ok() {
+    let mut cmd = Command::cargo_bin("false").unwrap();
+    cmd.assert().failure();
 }
