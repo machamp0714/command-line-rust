d024914ad37a709a44ed573121578005c9516fbb
machamp0714 <machamp0714@gmail.com>
2024-07-28 12:08:36 +0900

    Update receiving arguments

--- catr/src/lib.rs
+++ catr/src/lib.rs
@@ -1,9 +1,58 @@
+use clap::{App, Arg};
 use std::error::Error;
+
+#[derive(Debug)]
+#[allow(dead_code)]
+pub struct Config {
+    files: Vec<String>,
+    number_lines: bool,
+    number_nonblank_lines: bool,
+}
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
-pub fn run() -> MyResult<()> {
+pub fn run(config: Config) -> MyResult<()> {
     // ()はユニット型
-    println!("Hello, world!");
+    dbg!(config);
     Ok(())
+}
+
+pub fn get_args() -> MyResult<Config> {
+    let matches = App::new("catr")
+        .version("1.0")
+        .author("machamp0714")
+        .about("Rust cat")
+        .arg(
+            Arg::with_name("files")
+                .value_name("FILE")
+                .help("Input file(s)")
+                .multiple(true)
+                .default_value("-"),
+        )
+        .arg(
+            Arg::with_name("number")
+                .short("n")
+                .long("number")
+                .help("Number the output lines, starting at 1")
+                .conflicts_with("number_nonblank")
+                .takes_value(false),
+        )
+        .arg(
+            Arg::with_name("number_nonblank")
+                .short("b")
+                .long("number-nonblank")
+                .help("Number the output lines, starting at 1, omitting blank lines")
+                .takes_value(false),
+        )
+        .get_matches();
+
+    let files = matches.values_of_lossy("files").unwrap();
+    let number_lines = matches.is_present("number");
+    let number_nonblank_lines = matches.is_present("number_nonblank");
+
+    Ok(Config {
+        files,
+        number_lines,
+        number_nonblank_lines,
+    })
 }
