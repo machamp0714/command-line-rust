ad0031249bc0a9763514280b6f31d57e52f85812
machamp0714 <machamp0714@gmail.com>
2024-08-09 22:50:14 +0900

    Output args

---
+++ headr/src/lib.rs
@@ -0,0 +1,57 @@
+use clap::{Arg, App};
+use std::error::Error;
+
+#[derive(Debug)]
+pub struct Config {
+    files: Vec<String>,
+    count: String,
+    bytes: String,
+}
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+pub fn run(config: Config) -> MyResult<()> {
+    println!("{:?}", config);
+    Ok(())
+}
+
+pub fn get_args() -> MyResult<Config> {
+    let matches = App::new("headr")
+        .version("1.0.0")
+        .author("machamp0714")
+        .about("Rust head")
+        .arg(
+            Arg::with_name("files")
+                .value_name("FILE")
+                .default_value("-")
+                .multiple(true)
+        )
+        .arg(
+            Arg::with_name("lines")
+                .short("n")
+                .long("lines")
+                .value_name("LINES")
+                .default_value("10")
+                .conflicts_with("bytes")
+                .help("Print count lines of each of the specified files.")
+        )
+        .arg(
+            Arg::with_name("bytes")
+                .short("c")
+                .long("bytes")
+                .help("Print bytes of each of the specified files.")
+        )
+        .get_matches();
+
+    let files  = matches.values_of_lossy("files").unwrap();
+    let count = matches.value_of("count").unwrap();
+    let bytes = matches.value_of("bytes").unwrap();
+
+    let config = Config {
+        files,
+        count: String::from(count),
+        bytes: String::from(bytes)
+    };
+
+    Ok(config)
+}
