b377e498aa1d9dbadb2ad81d8f0906440117e143
machamp0714 <machamp0714@gmail.com>
2024-09-01 10:32:03 +0900

    Update use Derive API

--- wcr/src/main.rs
+++ wcr/src/main.rs
@@ -1,6 +1,33 @@
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(author, version, about)]
+pub struct Args {
+    #[arg(value_name = "FILE", default_value = "-")]
+    files: Vec<String>,
+    #[arg(
+        short = 'l',
+        long = "lines"
+    )]
+    lines: bool,
+    #[arg(
+        short = 'w',
+        long = "words"
+    )]
+    words: bool,
+    #[arg(
+        short = 'c',
+        long = "bytes"
+    )]
+    bytes: bool,
+    #[arg(
+        short = 'm',
+        long = "chars"
+    )]
+    chars: bool
+}
+
 fn main() {
-    if let Err(e) = wcr::get_args().and_then(wcr::run) {
-        eprintln!("Error: {}", e);
-        std::process::exit(1);
-    }
+    let args = Args::parse();
+    println!("{:?}", args);
 }
